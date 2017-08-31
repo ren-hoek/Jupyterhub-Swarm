d9ba459f767c6baf5c610257487fa46b4b84a9f4
ren-hoek <ren-hoek@users.noreply.github.com>
2017-08-31 18:36:19 +0100

    Updated config/jupyter_hub_config.py

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -25,38 +25,28 @@ c.SwarmSpawner.notebook_dir = notebook_dir
 mounts = [{'type': 'volume',
            'source': 'jupyterhub-user-{username}',
            'target': notebook_dir},
-	  {'type': 'bind',
+          {'type': 'bind',
            'source': '/etc/passwd',
            'target': '/etc/passwd'},
-	  {'type': 'bind',
+          {'type': 'bind',
            'source': '/etc/shadow',
            'target': '/etc/shadow'},
-	  {'type': 'bind',
+          {'type': 'bind',
            'source': '/etc/group',
            'target': '/etc/group'},
-	  {'type': 'bind',
+          {'type': 'bind',
            'source': '/etc/gshadow',
            'target': '/etc/gshadow'},
-	  {'type': 'bind',
+          {'type': 'bind',
            'source': '/etc/sudoers',
            'target': '/etc/sudoers'}
-	  ]
+        ]
 
 c.SwarmSpawner.container_spec = {
     # The command to run inside the service
     'args': ['/usr/local/bin/start-singleuser.sh',
-	     '--NotebookApp.default_url=/lab'],  # (string or list)
+        '--NotebookApp.default_url=/lab'],  # (string or list)
     'Image': 'jupyter/pip-notebook',
     'mounts': mounts
 }
 
-#c.SwarmSpawner.resource_spec = {
-    # (int) – CPU limit in units of 10^9 CPU shares.
-#    'cpu_limit': int(1 * 1e9),
-    # (int) – Memory limit in Bytes.
-#    'mem_limit': int(512 * 1e6),
-    # (int) – CPU reservation in units of 10^9 CPU shares.
-#    'cpu_reservation': int(1 * 1e9),
-    # (int) – Memory reservation in bytes
-#    'mem_reservation': int(512 * 1e6),
-#}
